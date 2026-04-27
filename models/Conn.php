eec0e44b445e319a6244388eccb1d2620d30b4d7
Enrico  Spinardi <167817620+EnricoSpinardi@users.noreply.github.com>
2026-04-27 14:40:50 -0300

    Add files via upload

---
+++ models/Conn.php
@@ -0,0 +1,13 @@
+<?php
+
+class Coon{
+    public static function getConexao(){
+        $servername = "localhost";
+        $username = "root";
+        $password = "";     
+        $dbname = "aula_php"; 
+        $conexao = new mysqli($servername, $username, $password, $dbname);
+        return $conexao;
+        }
+    }
+?>
